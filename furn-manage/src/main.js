be2749de85ffaeb2b7787a1a5958cea1f89c18f4
haomuren <827172670@qq.com>
2019-03-28 16:29:58 +0800

    write a index and router

--- furn-manage/src/main.js
+++ furn-manage/src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+import Vant from 'vant' // 引入已安装好的 Vant ui 这个插件
+import 'vant/lib/index.css'
 import App from './App.vue'
 import router from './router'
+import { Row, Col } from 'vant';
 
-Vue.config.productionTip = false
-
+Vue.use(Row).use(Col);
+const eventBus = new Vue();
+Vue.config.productionTip = false;
+Vue.use(Vant); // 引用Vant 这个插件
+Vue.prototype.$eventBus = eventBus;
 new Vue({
   router,
   render: h => h(App)
